a8d596da1a05cbb9a4685f3ec05de2a2f9c78be2
UtterDonkey <68461677+UtterDonkey@users.noreply.github.com>
2021-09-24 19:30:10 +0100

    Update SpeechRecognition.js

--- SpeechRecognition.js
+++ SpeechRecognition.js
@@ -1,9 +1,6 @@
 var SR = false;
 function speech() {
-  if (SR) {
-    console.log('Already Ongoing');
-    setCloud('type', 'ongoing');
-  } else {
+  if (!SR) {
 SR = true;
 setCloud('type', '...'); var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition; var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList; var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent; var colors = [ 'katie']; var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | '); var recognition = new SpeechRecognition(); var speechRecognitionList = new SpeechGrammarList(); speechRecognitionList.addFromString(grammar, 1); recognition.grammars = speechRecognitionList; recognition.continuous = false; recognition.lang = 'en-GB'; recognition.interimResults = false; recognition.maxAlternatives = 1; recognition.start(); console.log('Ready to receive a color command.'); recognition.onresult = function(event) { var color = event.results[0][0].transcript; setCloud('type', 'result'); setCloud('result', color); console.log('Result received: ' + color + '.'); SR = false;};recognition.onnomatch = function(event) { setCloud('type', 'error'); SR = false; }; recognition.onerror = function(event) { setCloud('type', 'error'); SR = false;}; setCloud('type', 'recording');
 }
