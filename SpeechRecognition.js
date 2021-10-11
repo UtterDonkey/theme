f81f0afd259dfdf57724dd47aac111619352954c
UtterDonkey <68461677+UtterDonkey@users.noreply.github.com>
2021-10-11 12:09:09 +0100

    Update SpeechRecognition.js

--- SpeechRecognition.js
+++ SpeechRecognition.js
@@ -5,6 +5,7 @@ SR = true;
 setCloud('type', '...'); var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition; var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList; var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent; var colors = [ 'katie', 'hey katie']; var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | '); var recognition = new SpeechRecognition(); var speechRecognitionList = new SpeechGrammarList(); speechRecognitionList.addFromString(grammar, 1); recognition.grammars = speechRecognitionList; recognition.continuous = false; recognition.lang = 'en-GB'; recognition.interimResults = false; recognition.maxAlternatives = 1; recognition.start(); console.log('Ready to receive a color command.'); recognition.onresult = function(event) { var color = event.results[0][0].transcript; setCloud('type', 'result'); setCloud('result', color); console.log('Result received: ' + color + '.'); SR = false;};recognition.onnomatch = function(event) { setCloud('type', 'error'); SR = false; }; recognition.onerror = function(event) { setCloud('type', 'error'); SR = false;}; setCloud('type', 'recording');
 }
 }
+var synth = window.speechSynthesis;
 var voices = [];
 
 function populateVoiceList() {
@@ -23,7 +24,7 @@ populateVoiceList();
 if (speechSynthesis.onvoiceschanged !== undefined) {
   speechSynthesis.onvoiceschanged = populateVoiceList;
 }
-var synth = window.speechSynthesis;
+
 function speak(text){
 
     if (text !== '') {
@@ -46,4 +47,3 @@ function speak(text){
     synth.speak(utterThis);
   }
 }
-
