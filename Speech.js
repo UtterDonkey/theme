4f70a3beb7120414bf083f3a0120c72eb3dbd615
UtterDonkey <68461677+UtterDonkey@users.noreply.github.com>
2021-10-12 13:02:52 +0100

    Update Speech.js

--- Speech.js
+++ Speech.js
@@ -53,8 +53,8 @@ class SpeechRecognition {
                 },
                               {
                     "opcode": "recording",
-                    "blockType": "boolean",
-                    "text": "recording?",
+                    "blockType": "reporter",
+                    "text": "recordin?",
                 },
             ],
 
