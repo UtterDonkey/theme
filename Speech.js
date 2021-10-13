3ea72410422336cada809c32e1cebe203acc7367
UtterDonkey <68461677+UtterDonkey@users.noreply.github.com>
2021-10-13 10:18:30 +0100

    Update Speech.js

--- Speech.js
+++ Speech.js
@@ -1,17 +1,19 @@
 const SpeechIcon = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSIjRkZGRkZGIj48cGF0aCBkPSJNMTIgMTRjMS42NiAwIDIuOTktMS4zNCAyLjk5LTNMMTUgNWMwLTEuNjYtMS4zNC0zLTMtM1M5IDMuMzQgOSA1djZjMCAxLjY2IDEuMzQgMyAzIDN6bTUuMy0zYzAgMy0yLjU0IDUuMS01LjMgNS4xUzYuNyAxNCA2LjcgMTFINWMwIDMuNDEgMi43MiA2LjIzIDYgNi43MlYyMWgydi0zLjI4YzMuMjgtLjQ4IDYtMy4zIDYtNi43MmgtMS43eiIvPjxwYXRoIGQ9Ik0wIDBoMjR2MjRIMHoiIGZpbGw9Im5vbmUiLz48L3N2Zz4K";
 const SpeechMenu = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNHB4IiBoZWlnaHQ9IjI0cHgiIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0iIzc1NzU3NSI+CiAgICA8cGF0aCBkPSJNMTIgMTRjMS42NiAwIDIuOTktMS4zNCAyLjk5LTNMMTUgNWMwLTEuNjYtMS4zNC0zLTMtM1M5IDMuMzQgOSA1djZjMCAxLjY2IDEuMzQgMyAzIDN6bTUuMy0zYzAgMy0yLjU0IDUuMS01LjMgNS4xUzYuNyAxNCA2LjcgMTFINWMwIDMuNDEgMi43MiA2LjIzIDYgNi43MlYyMWgydi0zLjI4YzMuMjgtLjQ4IDYtMy4zIDYtNi43MmgtMS43eiIvPgogICAgPHBhdGggZD0iTTAgMGgyNHYyNEgweiIgZmlsbD0ibm9uZSIvPgo8L3N2Zz4K";
 var record = false;
+var ready = false;
 var word = ' ';
 var listenFor = ' ';
 var SR = false;
 var voice = ' ';
+var recognition = ' ';
 function speech(lfor) {
   if (!SR) {
 SR = true;
 return new Promise(function(resolve, reject) {
-   
-
- var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition; var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList; var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent; var colors = [ lfor]; var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | '); var recognition = new SpeechRecognition(); var speechRecognitionList = new SpeechGrammarList(); speechRecognitionList.addFromString(grammar, 1); recognition.grammars = speechRecognitionList; recognition.continuous = false; recognition.lang = 'en-GB'; recognition.interimResults = false; recognition.maxAlternatives = 1; recognition.start(); recognition.onresult = function(event) { var color = event.results[0][0].transcript; if ( record ) {voice = color;} listenFor = color; resolve(color); record = false; SR = false; speech(word); word = ' ';};recognition.onnomatch = function(event) {  resolve('[error]'); SR = false; if ( record ) {voice = '[Not Recognised]';} record = false;  speech(word); word = ' ';}; recognition.onerror = function(event) {  resolve('[error]'); if ( record ) {voice = '[error]';} SR = false; record = false; speech(word); word = ' ';};
+   if ( record) {ready = true;}
+var using = record;
+ var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition; var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList; var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent; var colors = [ lfor]; var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | '); recognition = new SpeechRecognition(); var speechRecognitionList = new SpeechGrammarList(); speechRecognitionList.addFromString(grammar, 1); recognition.grammars = speechRecognitionList; recognition.continuous = false; recognition.lang = 'en-GB'; recognition.interimResults = false; recognition.maxAlternatives = 1; recognition.start(); recognition.onresult = function(event) { var color = event.results[0][0].transcript; if ( using ) {voice = color; record = false; ready = false;} listenFor = color; resolve(color);  SR = false; speech(word);  word = ' ';};recognition.onnomatch = function(event) {  resolve('[error]'); SR = false; if ( using ) {voice = '[Not Recognised]'; record = false; ready = false;}  speech(word); word = ' ';}; recognition.onerror = function(event) {  resolve('[error]'); if ( using ) {voice = '[error]'; record = false; ready = false} SR = false; ; speech(word); word = ' ';};
 });
 }
 }
@@ -86,14 +88,16 @@ class SpeechRecognition {
     }
 
     record() {
-
+recognition.stop(); recognition.start(); 
         record = true;
         
 
     }
 hear({text}) {
+recognition.stop(); recognition.start(); 
        record = true;
        word = text;
+
 }
 
 
@@ -102,7 +106,7 @@ hear({text}) {
         return speech;
     }
   recording() {
-   let recording = record;
+   let recording = ready;
     return recording;
   }
 listen({string}) {
