79a70dcb8c2f763bf16ae7572a1a745cce3a3dbc
UtterDonkey <68461677+UtterDonkey@users.noreply.github.com>
2021-12-27 10:25:03 +0000

    Update fix.js

--- fix.js
+++ fix.js
@@ -1,6 +1,10 @@
 function catchLoad(){
 if(vm.assets.length > 0){
-document.getElementsByClassName('monitor-value')[0].style.height = '0.75rem'
+
+for (var i = 0; i < document.getElementsByClassName('monitor-value').length; i++) {
+document.getElementsByClassName('monitor-value')[i].style.height = '0.75rem'
+}
+
 }
 setTimeout(catchLoad, 10)
 }
